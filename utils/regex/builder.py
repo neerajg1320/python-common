03ef59c7b4bfbe8ad4ec16eea92be0cd8ff1b88e
Neeraj Gupta <neerajgupta.mbox@gmail.com>
2022-08-31 19:05:50 +0530

    Created a function for getting lines with offsets given a text

--- utils/regex/builder.py
+++ utils/regex/builder.py
@@ -409,8 +409,10 @@ class RegexTextProcessor:
 
         # TBD: Can be made as a routine
         # We leave the \n out of the match even though we match the whole line
-        result = regex_apply_on_text('^.*$', self.data, flags={"multiline": 1})
-        self.all_lines_with_offsets = result["matches"]
+        # result = regex_apply_on_text('^.*$', self.data, flags={"multiline": 1})
+        # self.all_lines_with_offsets = result["matches"]
+        from utils.regex.patterns import get_line_matches_from_text
+        self.all_lines_with_offsets = get_line_matches_from_text(self.data)
 
         regex_str = self.regex_token_set.regex_str()
         pattern = re.compile(regex_str)
--- utils/regex/patterns.py
+++ utils/regex/patterns.py
@@ -1,4 +1,6 @@
 import re
+from ..regex_utils import regex_apply_on_text
+
 
 # Used to match a valid string in regex comment pattern
 REGEX_COMMENT_PATTERN_ESCAPED = r"^\(\?#.*\)$"
@@ -18,3 +20,11 @@ def is_whitespace(text):
 
 def get_regex_comment_pattern():
     return REGEX_COMMENT_PATTERN
+
+
+def get_line_matches_from_text(text, newline_include=False):
+    line_regex_str = "^.*$"
+    if newline_include:
+        "".join([line_regex_str, "\\n"])
+    result = regex_apply_on_text(line_regex_str, text, flags={"multiline": 1})
+    return result["matches"]
