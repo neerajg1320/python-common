d2a368a861123acbde70f7570eb859b095c0859b
Neeraj Gupta <neerajgupta.mbox@gmail.com>
2022-09-09 07:55:03 +0530

    Refactore the md5 hash generation code

--- utils/md5/hash.py
+++ utils/md5/hash.py
@@ -1,8 +1,12 @@
 import hashlib
 
 
-def get_md5_hash(input_str, max_length=0):
-    hash_digest = hashlib.md5(input_str.encode('utf-8')).hexdigest()
+def get_md5_hash_from_str(input_str, max_length=0):
+    return get_md5_hash_from_bytes(input_str.encode('utf-8'), max_length=max_length)
+
+
+def get_md5_hash_from_bytes(input_bytes, max_length=0):
+    hash_digest = hashlib.md5(input_bytes).hexdigest()
 
     if max_length > 0:
         hash_digest = hash_digest[0:max_length]
